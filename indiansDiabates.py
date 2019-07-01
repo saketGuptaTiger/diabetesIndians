13bec022858f041eb8a39dc590a9b34ca41482ae
saketGuptaTiger <saketguptaabc@gmail.com>
2019-06-30 19:25:37 -0500

    Add files via upload

--- indiansDiabates.py
+++ indiansDiabates.py
@@ -39,3 +39,27 @@ loss, score = model.evaluate(X, y)
 
 print("\n%s: %.2f%%" % (model.metrics_names[1], score*100))
 
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+ 
+# later...
+ 
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
